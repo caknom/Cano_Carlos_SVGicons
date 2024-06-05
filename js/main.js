2646c0d3b4df21faeeb7d1b44d8905fd4571975b
Carlos Cano <85541435+caknom@users.noreply.github.com>
2024-06-05 08:09:46 -0400

    Java file working
    In this commit, I script the Java file and it is working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log("JS file connected");
+
+const iconCon = document.querySelectorAll(".icon-con svg");
+console.log(iconCon);
+
+function logId() {
+    console.log(this.id);
+}
+
+iconCon.forEach(function(iconConClick) {
+    iconConClick.addEventListener("click", logId);
+})
